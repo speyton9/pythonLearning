8a759b92aa19037977ab03c7ed10f380c0f911a2
Steven Peyton <sjpeyton9@gmail.com>
2024-09-11 13:12:41 -0400

    Created player object and tested

--- test_main.py
+++ test_main.py
@@ -1,2 +1,12 @@
+class Player:
+    def __init__(self, name, number, position):
+        self.name = name
+        self.number = number
+        self.postion = position
+
+    def action(self):
+        return f"{self.name} is a {self.postion} whose number is {self.number}"
+
 if __name__ == "__main__":
-    print("hello World")
+    test_player = Player("Steven", 9, "D")
+    print(test_player.action())
