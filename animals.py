efc1cc145e5359a6dfe8a82d9b590b7d7edcb11c
Steven Peyton <sjpeyton9@gmail.com>
2024-09-11 13:32:58 -0400

    Object created

--- animals.py
+++ animals.py
@@ -1,2 +1,11 @@
+class Animal:
+    def __init__(self, legs, name):
+        self.legs = legs
+        self.name = name
+
+    def describe(self):
+        return f"{self.name} has {self.legs} legs"
+
 if __name__ == "__main__":
-    print("hello World")
+    test_animal = Animal(4, "Sparky")
+    print(test_animal.describe())
