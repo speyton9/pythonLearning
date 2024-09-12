74e82c310898d6e4b24bc1e6c78ad79e0da2dc43
Steven Peyton <sjpeyton9@gmail.com>
2024-09-12 09:22:58 -0400

    Create team object

--- hockey_class.py
+++ hockey_class.py
@@ -1,2 +1,12 @@
+class Team:
+    def __init__(self, team_name, city, mascot):
+        self.team_name = team_name
+        self.city = city
+        self.mascot = mascot
+
+    def team_info(self):
+        return f"The {self.city} {self.team_name} mascot is named {self.mascot}"
+
 if __name__ == "__main__":
-    print("HW")
+    test_team = Team("Flyers", "Philadelphia", 'Gritty')
+    print(test_team.team_info())
