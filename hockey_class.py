2c299ad5ddedbc65285e92b88ddd6d5f882d0f55
Steven Peyton <sjpeyton9@gmail.com>
2024-09-12 09:51:33 -0400

    create player child class

--- hockey_class.py
+++ hockey_class.py
@@ -6,7 +6,21 @@ class Team:
 
     def team_info(self):
         return f"The {self.city} {self.team_name} mascot is named {self.mascot}"
+    
+class Player(Team):
+    def __init__(self, player_name, number, age, position, team_name, city, mascot):
+        super().__init__(team_name, city, mascot)
+        self.player_name = player_name
+        self.number = number
+        self.age = age
+        self.position = position
+
+    def player_info(self):
+        return f"{self.player_name} is a {self.age} year old {self.position} and wears #{self.number} on the {self.city} {self.team_name}"
 
 if __name__ == "__main__":
     test_team = Team("Flyers", "Philadelphia", 'Gritty')
-    print(test_team.team_info())
+    #print(test_team.team_info())
+    test_player = Player("Giroux", 28, 36, "Center", "Flyers", "Philadelphia", 'Gritty')
+    print(test_player.player_info())
+    print(test_player.team_info())
